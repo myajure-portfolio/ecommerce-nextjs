8348efa59ee9ce5704fa4d36f0e3fac3e6f6d57e
Mariangel Yajure <mariangel.yajure@gmail.com>
2025-07-25 22:29:21 -0400

    adding seed and prisma orm

---
+++ prisma/migrations/20250725235535_product_model/migration.sql
@@ -0,0 +1,55 @@
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('men', 'women', 'kid', 'unisex');
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "sizes" "Size"[],
+    "price" DECIMAL(12,2) NOT NULL DEFAULT 0,
+    "rating" DECIMAL(3,2) NOT NULL DEFAULT 0,
+    "numReviews" INTEGER NOT NULL DEFAULT 0,
+    "isFeatured" BOOLEAN NOT NULL DEFAULT false,
+    "banner" TEXT,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductImage" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "ProductImage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_slug_idx" ON "Product"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductImage" ADD CONSTRAINT "ProductImage_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/seed/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "../generated/prisma";
+import { categories, products } from "./sample-data";
+
+async function main() {
+  const prisma = new PrismaClient();
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  try {
+    await prisma.category.createMany({
+      data: categories.map((category) => ({
+        name: category,
+      })),
+    });
+
+    const categoriesMap = Object.fromEntries(
+      (await prisma.category.findMany()).map((category) => [
+        category.name,
+        category.id,
+      ])
+    );
+
+    for (const product of products) {
+      const { category, images, ...productData } = product;
+
+      const createdProduct = await prisma.product.create({
+        data: {
+          ...productData,
+          categoryId: categoriesMap[category],
+        },
+      });
+
+      await prisma.productImage.createMany({
+        data: product.images.map((imageUrl: string) => ({
+          url: imageUrl,
+          productId: createdProduct.id,
+        })),
+      });
+    }
+
+    console.log("Database seeded successfully with sample data.");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
